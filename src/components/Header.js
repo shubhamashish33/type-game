2f50385a4684562964f60340e4281fba02ac05d1
Shubham Ashish <78084828+shubhamashish33@users.noreply.github.com>
2021-09-25 09:07:16 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
   const [isscore, setScore] = useState(false);
 
   //Fetch API
-  const RANDOM_QUOTE_API_URL = "http://api.quotable.io/random";
+  const RANDOM_QUOTE_API_URL = "https://api.quotable.io/random";
   const quoteDisplayElement = document.getElementById("quoteDisplay");
   const quoteInputElement = document.getElementById("quoteInput");
   const timerElement = document.getElementById("timer");
